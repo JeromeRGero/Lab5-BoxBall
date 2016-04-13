66e9d2a75225419d2fabf0e31943615fb54dfe97
Jerome Gero <JeromeRGero@gmail.com>
2016-04-13 17:27:26 -0400

    BoxBall
    Completed. Finally.

--- BallDemo.java
+++ BallDemo.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.util.ArrayList;
 
 /**
  * Class BallDemo - a short demonstration showing animation with the 
@@ -11,6 +12,7 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
+    private final int BUMPER = 5;
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -18,6 +20,56 @@ public class BallDemo
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+    }
+    
+    public void drawCanvas(Canvas canvas)
+    {
+        Double height = canvas.getSize().getHeight();  
+        Double width = canvas.getSize().getWidth();  
+        
+        //Does it work?
+        canvas.setVisible(true);  
+        
+        //Draw 4 lines in which form a rectangle and holy hell was there really no easier way to do this look at the insane amount of code, jesus freaking-
+        canvas.drawLine(BUMPER, height.intValue() - BUMPER, width.intValue() - BUMPER, height.intValue() - BUMPER);  
+        canvas.drawLine(BUMPER, BUMPER, width.intValue() - BUMPER, BUMPER);  
+        canvas.drawLine(BUMPER, BUMPER, BUMPER, height.intValue() - BUMPER);  
+        canvas.drawLine(width.intValue() - BUMPER, BUMPER, width.intValue() - BUMPER, height.intValue() - BUMPER);  
+    }
+    
+    public void boxBounce(int Balls)
+    {
+        if (Balls < 0) 
+        {  
+            System.out.println("The heck man, cut the crap out!");  
+            return;  
+        } 
+        int BALLSSS = Balls - 1;
+        int x = 0;  
+        ArrayList<boxBall> balls = new ArrayList<>();  
+        drawCanvas(myCanvas);  
+   
+        //Uses an Array list for balls
+        for (int i = 0; i <= BALLSSS; i++) 
+        {  
+            balls.add(new boxBall(myCanvas, BUMPER));  
+        }  
+   
+        //draw balls from the Array
+        for (boxBall ball : balls) 
+        {  
+        ball.draw();  
+        }  
+        
+        while (x <= 100) //sets the timer for how long the balls are going to be moving
+        {  
+            myCanvas.wait(50); // small delay  
+            for (boxBall ball : balls) 
+            {  
+                ball.move();  
+            }  
+            x++;  
+        }
     }
 
     /**
---
+++ __SHELL6.java
@@ -0,0 +1,11 @@
+
+public class __SHELL6 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\Jerome\\Documents\\GitHub\\Lab5-BoxBall");
+final BallDemo ballDemo1 = (BallDemo)__bluej_runtime_scope.get("ballDemo1");
+
+
+int __bluej_param0 = 2000000;
+ballDemo1.boxBounce(__bluej_param0);
+
+}}
